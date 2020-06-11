4fe84818253c55e530933d981a74e11583f92bb4
Petro Yuskiv <yuskiv.pm@gmail.com>
2020-06-11 17:16:53 +0300

    refactor: hw2 - async

--- hw2/contacts.js
+++ hw2/contacts.js
@@ -1,51 +1,49 @@
 const fs = require('fs');
 const path = require('path');
 
-const contactPath = path.join('./db', 'contacts.json');
+const contactPath = path.join(__dirname, 'db', 'contacts.json');
 
+// helpers
 const getMaxId = contacts => contacts.reduce((maxId, { id }) => Math.max(maxId, id), 0) + 1;
-
 const prepareData = contacts => JSON.stringify(contacts, null, 2);
-
 const getIndexById = (contacts, contactId) => contacts.findIndex(({ id }) => id == contactId);
-
 const listContacts = () => fs.promises.readFile(contactPath, 'utf8').then(data => JSON.parse(data));
 
+// get all contacts
 const getContactById = contactId => listContacts().then(contacts => contacts.find(({ id }) => id == contactId));
 
-const removeContact = contactId =>
-  listContacts().then(contacts => {
-    const contactIndex = getIndexById(contacts, contactId);
-    if (contactIndex < 0) {
-      return null;
-    }
-    contacts.splice(contactIndex, 1);
-    return fs.promises.writeFile(contactPath, prepareData(contacts)).then(() => true);
-  });
-
-function addContact(name, email, phone) {
-  let newItem;
-  return listContacts()
-    .then(contacts =>
-      fs.promises.writeFile(
-        contactPath,
-        prepareData(contacts.concat((newItem = { id: getMaxId(contacts), name, email, phone })))
-      )
-    )
-    .then(() => newItem);
+// get contact by id
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const contactIndex = getIndexById(contacts, contactId);
+  const result = contactIndex >= 0;
+  if (result) {
+    await fs.promises.writeFile(contactPath, prepareData(contacts.filter((value, index) => index !== contactIndex)));
+  }
+  return result;
 }
 
-function updateContact(contactId, update) {
+// add new contact
+async function addContact(name, email, phone) {
+  let newItem;
+  const contacts = await listContacts();
+  await fs.promises.writeFile(
+    contactPath,
+    prepareData(contacts.concat((newItem = { id: getMaxId(contacts), name, email, phone })))
+  );
+  return newItem;
+}
+
+// update existing contact
+async function updateContact(contactId, update) {
   let resultContact;
-  return listContacts()
-    .then(contacts => {
-      const contactIndex = getIndexById(contacts, contactId);
-      if (contactIndex >= 0) {
-        contacts[contactIndex] = resultContact = { ...contacts[contactIndex], ...update };
-        fs.promises.writeFile(contactPath, prepareData(contacts));
-      }
-    })
-    .then(() => resultContact);
+  const contacts = await listContacts();
+  const contactIndex = getIndexById(contacts, contactId);
+  if (contactIndex >= 0) {
+    resultContact = Object.assign(contacts[contactIndex], update);
+    await fs.promises.writeFile(contactPath, prepareData(contacts));
+  }
+  return resultContact;
 }
 
 module.exports = {
--- hw2/index.js
+++ hw2/index.js
@@ -5,33 +5,31 @@ const { listContacts, getContactById, addContact, removeContact, updateContact }
 invokeAction(argv);
 
 function invokeAction({ action, id, name, email, phone }) {
-  let logThis
+  let logThis;
   switch (action) {
     case 'list':
-      logThis = listContacts()
-      break
+      logThis = listContacts();
+      break;
 
     case 'get':
-      logThis = getContactById(id)
-      break
+      logThis = getContactById(id);
+      break;
 
     case 'add':
-      logThis = addContact(name, email, phone)
-      break
+      logThis = addContact(name, email, phone);
+      break;
 
     case 'remove':
-      logThis = removeContact(id).then(() => 'Item removed')
-      break
+      logThis = removeContact(id).then(() => 'Item removed');
+      break;
 
     default:
       return startServer();
-      
   }
-  logThis.then(data => console.table(data || '')).catch(err => console.warn(err))
+  logThis.then(data => console.table(data || '')).catch(err => console.warn(err));
 }
 
-function startServer(){
-  
+function startServer() {
   const PORT = 3000;
   const FIELD_NAMES = ['name', 'email', 'phone'];
 
@@ -46,103 +44,91 @@ function startServer(){
   const response_any = (response, message, code = 200) => response.status(code).send(message);
   const response_404 = response => response_any(response, MSG_NOT_FOUND, 404);
   const response_500 = (error, response) => response_any(response, { error }, 500);
+  const catchExeption = (response, promise) => promise.catch(err => response_500(err, response));
 
   function validateBody({ body = {} } = {}) {
     const error = [];
     const contact = {};
-    fields = FIELD_NAMES.map(name => body[name] ? contact[name] = body[name] : error.push(name));
+    fields = FIELD_NAMES.map(name => (body[name] ? (contact[name] = body[name]) : error.push(name)));
     return { error, fields, contact };
   }
 
-  const catchExeption = (response, promise) => promise.catch(err => response_500(err, response));
-  
   const express = require('express');
   const app = express();
 
-  app.use(express.json());
-  app.use(require('cors')());
-  app.use(require('morgan')('combined'));
+  app.use(express.json(), require('cors')(), require('morgan')('combined'));
 
-  /*
-  @ GET /api/contacts
-  ничего не получает
-  вызывает функцию listContacts для работы с json-файлом contacts.json
-  возвращает массив всех контактов в json-формате со статусом 200
+  /* @ GET /api/contacts
+  Call listContacts().
+  Returns the code 200 and JSON with an array of all contacts.
   */
   app.get(API_CONTACTS, (request, response) =>
-    catchExeption(response, 
-      listContacts().then(contacts => 
-        response_any(response, contacts)
-      )
+    catchExeption(
+      response,
+      listContacts().then(contacts => response_any(response, contacts))
     )
   );
 
-  /*
-  @ GET /api/contacts/:contactId
-  Не получает body
-  Получает параметр contactId
-  вызывает функцию getById для работы с json-файлом contacts.json
-  если такой id есть, возвращает обьект контакта в json-формате со статусом 200
-  если такого id нет, возвращает json с ключом "message": "Not found" и статусом 404
+  /* @ GET /api/contacts/:contactId
+  Receives contactId.
+  Calls getById().
+  If contactId exists it returns the code 200 and JSON with contact.
+  Else it returns the code 404 and JSON {"message": "Not found"}
   */
   app.get(API_CONTACT_BY_ID, (request, response) =>
-    catchExeption(response, 
-      getContactById(request.params[CONTACT_ID]).then(contact => 
+    catchExeption(
+      response,
+      getContactById(request.params[CONTACT_ID]).then(contact =>
         contact ? response_any(response, contact) : response_404(response)
       )
     )
   );
 
-  /*
-  @ POST /api/contacts
-  Получает body в формате {name, email, phone}
-  Если в body нет каких-то обязательных полей, возарщает json с ключом {"message": "missing required name field"} и статусом 400
-  Если с body все хорошо, добавляет уникальный идентификатор в обьект контакта
-  Вызывает функцию addContact() для сохранения контакта в файле contacts.json
-  По результату работы функции возвращает обьект с добавленным id {id, name, email, phone} и статусом 201
+  /* @ POST /api/contacts
+  Receives body with JSON { name, email, phone }.
+  If received data is invalid it returns the code 400 with JSON {"message": "missing required name field"}.
+  Else it calls addContact(), returns the code 201 and JSON with created contact.
   */
   app.post(API_CONTACTS, (request, response) => {
     const { error, fields } = validateBody(request);
     if (error.length) {
       return response_any(response, { message: `missing required ${error} field${error.length > 1 ? 's' : ''}` }, 400);
     }
-    catchExeption(response, 
-      addContact(...fields).then(newContact => 
-        response_any(response, newContact, 201)
-      )
+    catchExeption(
+      response,
+      addContact(...fields).then(newContact => response_any(response, newContact, 201))
     );
   });
 
-  /*
-  @ DELETE /api/contacts/:contactId
-  Не получает body
-  Получает параметр contactId
-  вызывает функцию removeContact для работы с json-файлом contacts.json
-  если такой id есть, возвращает json формата {"message": "contact deleted"} и статусом 200
-  если такого id нет, возвращает json с ключом "message": "Not found" и статусом 404
+  /* @ DELETE /api/contacts/:contactId
+  Receives contactId.
+  Calls removeContact().
+  If contactId exists, it returns the code 200 and JSON {"message": "contact deleted"}.
+  Else it returns the code 404 and JSON {"message": "Not found"}
   */
   app.delete(API_CONTACT_BY_ID, (request, response) =>
-    catchExeption(response, 
-      removeContact(request.params[CONTACT_ID]).then(result => 
-        (result ? response_any(response, MSG_CONTACT_DELETED) : response_404(response))
+    catchExeption(
+      response,
+      removeContact(request.params[CONTACT_ID]).then(result =>
+        result ? response_any(response, MSG_CONTACT_DELETED) : response_404(response)
       )
     )
   );
 
-  /*
-  @ PATCH /api/contacts/:contactId
-  Получает body в json-формате c обновлением любых полей name, email и phone
-  Если body нет, возарщает json с ключом {"message": "missing fields"} и статусом 400
-  Если с body все хорошо, вызывает функцию updateContact(id) (напиши ее) для обновления контакта в файле contacts.json
-  По результату работы функции возвращает обновленный обьект контакта и статусом 200. В противном случае, возвращает json с ключом "message": "Not found" и статусом 404
+  /* @ PATCH /api/contacts/:contactId
+  Receives JSON with a new value for any fields (name, email, phone).
+  If the request body is invalid it returns the code 400 and JSON {"message": "missing fields"}.
+  If such contact doesn't exist it returns the code 404 and JSON {"message": "Not found"}.
+  Else it calls updateContact(id) function, returns the code 200 and JSON with updated contact.
   */
   app.patch(API_CONTACT_BY_ID, (request, response) => {
     const { error, contact } = validateBody(request);
     if (error.length === FIELD_NAMES.length) {
       return response_any(response, MSG_MISSING_FIELDS, 400);
     }
-    catchExeption(response, updateContact(request.params[CONTACT_ID], contact)
-      .then(updatedContact => 
+    catchExeption(
+      response,
+      updateContact(request.params[CONTACT_ID], contact).then(updatedContact =>
         updatedContact ? response_any(response, updatedContact) : response_404(response)
       )
     );
